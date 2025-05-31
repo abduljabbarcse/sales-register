a3116c319c8015ed2181f16a261de9bfb0811189
abduljabbarcse <abduljabbarcse2001@gmail.com>
2025-05-31 19:44:57 +0530

    commit

--- src/main.jsx
+++ src/main.jsx
@@ -5,15 +5,15 @@ import { ThemeProvider } from '@mui/material/styles';
 import theme from './theme/theme';
 import { Provider } from 'react-redux';
 import store from './redux/store';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <ThemeProvider theme={theme}>
-        <BrowserRouter basename='/sales-register/'>
+        <HashRouter >
           <App />
-        </BrowserRouter>
+        </HashRouter>
       </ThemeProvider>
     </Provider>
   </React.StrictMode>
